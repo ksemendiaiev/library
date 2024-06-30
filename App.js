0856200596f24a89c20c89c890aee42a6ce64d6e
ksemendiaiev <semendiaievkyrylo@gmail.com>
2024-06-30 20:58:42 +0300

    Refactor to classes, improve the structure of the code

--- App.js
+++ App.js
@@ -1,116 +1,138 @@
-const myLibrary = [
-    {title: "Last Wish", authorName: 'Andrzej Sapkowski', genre: "fantasy", isRead: false},
-    {title: "Game of Thrones", authorName: 'George R. R. Martin', genre: "fantasy", isRead: false}
-];
-const booksContainer = document.getElementById("books-container");
-const addNewBookToLibrary = document.getElementById('show-dialog');
-const dialog = document.getElementById('dialog');
-const CloseBtn = dialog.querySelector('#js-close');
+class Book {
+    constructor(title, authorName, genre, isRead = false) {
+        this.title = title;
+        this.authorName = authorName;
+        this.genre = genre;
+        this.isRead = isRead;
+    }
+}
+class Library {
+    constructor() {
+        this.books = [
+            new Book("Last Wish", 'Andrzej Sapkowski', "fantasy"),
+            new Book("Game of Thrones", 'George R. R. Martin', "fantasy")
+        ]
 
+    }
 
-const renderBooks = (library) => {
-    booksContainer.innerHTML = '';
+    renderBooks(container) {
+        container.innerHTML = '';
+        this.books.map((book) => {
+            const bookCard = document.createElement('div');
+            bookCard.classList.add('book-card');
 
-    library.map((book) => {
-        const bookCard = document.createElement('div');
-        bookCard.classList.add('book-card');
+            const infoDiv = document.createElement('div');
+            infoDiv.classList.add('info-div');
 
-        const infoDiv = document.createElement('div');
-        infoDiv.classList.add('info-div');
+            const buttonsDiv = document.createElement('div');
+            buttonsDiv.classList.add('buttons-div');
 
-        const buttonsDiv = document.createElement('div');
-        buttonsDiv.classList.add('buttons-div');
+            const title = document.createElement('h2');
+            title.classList.add('book-title');
+            title.textContent = book.title;
 
-        const title = document.createElement('h2');
-        title.classList.add('book-title');
-        title.textContent = book.title;
+            const authorName = document.createElement('p');
+            authorName.classList.add('author-name');
+            authorName.textContent = book.authorName;
 
-        const authorName = document.createElement('p');
-        authorName.classList.add('author-name');
-        authorName.textContent = book.authorName;
+            const genre = document.createElement('p');
+            genre.classList.add('genre');
+            genre.textContent = book.genre;
 
-        const genre = document.createElement('p');
-        genre.classList.add('genre');
-        genre.textContent = book.genre;
+            const isRead = document.createElement('input');
+            isRead.setAttribute('type', 'checkbox');
+            isRead.classList.add('isread-checkbox');
+            isRead.checked = book.isRead
 
-        const isRead = document.createElement('input');
-        isRead.setAttribute('type', 'checkbox');
-        isRead.classList.add('isread-checkbox');
-        isRead.checked = book.isRead
+            const readButton = document.createElement('button');
+            readButton.classList.add('read-button');
+            readButton.textContent = 'Read';
+            readButton.style.backgroundColor = 'rgb(50,205,50)';
 
-        const readButton = document.createElement('button');
-        readButton.classList.add('read-button');
-        readButton.textContent = 'Read';
-        readButton.style.backgroundColor = 'rgb(50,205,50)';
+            const notReadButton = document.createElement('button');
+            notReadButton.classList.add('read-button');
+            notReadButton.textContent = "Didn't read";
+            notReadButton.style.backgroundColor = "rgb(255,0,0)"
 
-        const notReadButton = document.createElement('button');
-        notReadButton.classList.add('read-button');
-        notReadButton.textContent = "Didn't read";
-        notReadButton.style.backgroundColor = "rgb(255,0,0)"
+            const deleteButton = document.createElement('button');
+            deleteButton.classList.add('delete-button');
+            deleteButton.textContent = 'Delete'
 
-        const deleteButton = document.createElement('button');
-        deleteButton.classList.add('delete-button');
-        deleteButton.textContent = 'Delete'
+            deleteButton.addEventListener('click', () => {
+                this.removeBook(book.title);
+                container.removeChild(bookCard);
+            })
 
-        deleteButton.addEventListener('click', () => {
-            const index = myLibrary.findIndex(item => item.title === book.title);
-            if (index !== -1) {
-                myLibrary.splice(index, 1);
+            infoDiv.appendChild(title);
+            infoDiv.appendChild(authorName);
+            infoDiv.appendChild(genre);
+
+            if (isRead.checked) {
+                buttonsDiv.appendChild(readButton);
+
+            } else if (!isRead.checked) {
+                buttonsDiv.appendChild(notReadButton);
             }
-            booksContainer.removeChild(bookCard);
-        })
+            buttonsDiv.appendChild(deleteButton);
 
-        infoDiv.appendChild(title);
-        infoDiv.appendChild(authorName);
-        infoDiv.appendChild(genre);
+            bookCard.appendChild(infoDiv);
+            bookCard.appendChild(buttonsDiv);
 
-        if (isRead.checked) {
-            buttonsDiv.appendChild(readButton);
-        } else if (!isRead.checked) {
-            buttonsDiv.appendChild(notReadButton);
-        }
-        buttonsDiv.appendChild(deleteButton);
+            container.appendChild(bookCard);
+        });
+    }
 
-        bookCard.appendChild(infoDiv);
-        bookCard.appendChild(buttonsDiv);
+    addBook(book){
+        this.books.push(book);
+    }
 
-        booksContainer.appendChild(bookCard);
-    });
+    removeBook(title){
+        this.books = this.books.filter(book => book.title !== title)
+    }
 }
-const addBookToLibrary = () => {
-    addNewBookToLibrary.addEventListener("click", () => {
-        dialog.showModal();
-    });
 
-    CloseBtn.addEventListener('click', (e) => {
-        e.preventDefault();
-        const titleBookInput = document.getElementById('titleBookInput').value;
-        const authorInput = document.getElementById('authorInput').value;
-        const genreInput = document.getElementById('genreInput').value;
-        const isReadValue = document.getElementById('isRead').checked;
+class DOMHandler {
+    constructor() {
+        this.booksContainer = document.getElementById("books-container");
+        this.addNewBookToLibrary = document.getElementById('show-dialog');
+        this.dialog = document.getElementById('dialog');
+        this.CloseBtn = this.dialog.querySelector('#js-close');
+    }
 
-        const book = {
-            title: titleBookInput,
-            authorName: authorInput,
-            genre: genreInput,
-            isRead: isReadValue
-        };
+    initialize(library) {
+        this.addNewBookToLibrary.addEventListener("click", () => {
+            domHandler.dialog.showModal();
+        });
 
-        if(book.title === '' || book.authorName === '' || book.genre === ''){
-            return false;
-        }
-        else{
-            myLibrary.push(book);
-            renderBooks(myLibrary);
-            dialog.close();
-        }
-        // myLibrary.push(book);
-        // renderBooks(myLibrary);
-        // dialog.close();
-    });
+        this.CloseBtn.addEventListener('click', (e) => {
+            e.preventDefault();
+            const titleBookInput = document.getElementById('titleBookInput').value;
+            const authorInput = document.getElementById('authorInput').value;
+            const genreInput = document.getElementById('genreInput').value;
+            const isReadValue = document.getElementById('isRead').checked;
+
+            const book = new Book(titleBookInput, authorInput, genreInput, isReadValue)
+            if (book.title === '' || book.authorName === '' || book.genre === '') {
+                return false;
+            } else {
+                library.addBook(book);
+                library.renderBooks(this.booksContainer);
+                this.dialog.close();
+            }
+        });
+        library.renderBooks(this.booksContainer);
+
+    }
+
 }
-addBookToLibrary();
-renderBooks(myLibrary);
+
+const library = new Library();
+const domHandler = new DOMHandler();
+domHandler.initialize(library)
+
+
+
+
 
 
 
